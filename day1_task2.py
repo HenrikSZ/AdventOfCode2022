ad9b9a184a4b6d8380801df0e6e8d9163e20835c
Henrik Zimmermann <henrik@zi-data.com>
2022-12-01 00:15:57 -0500

    Completed day 1, prepared for day 2.

--- day1_task2.py
+++ day1_task2.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# Day 1, Task 1                                                               #
+# Day 1, Task 2                                                               #
 ###############################################################################
 
 import aoc_util
@@ -23,16 +23,18 @@ data_str = """1000
 
 def task(data_set: list[str]) -> int:
     counter = 0
-    max_counter = 0
+    max_list = []
 
     for s in data_set:
         if s == "":
-            max_counter = max(counter, max_counter)
+            max_list += [counter]
             counter = 0
         else:
             counter += int(s)
 
-    return max_counter
+    max_list += [counter]
+
+    return sum(sorted(max_list)[-3:])
 
 
 aoc_util.run_with_data_str(task, data_str)
